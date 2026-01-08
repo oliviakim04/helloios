e76fc3d186f009ecd7b0b81fda2d61a9324f45d4
Olivia Kim <chaeyoon1116@gmail.com>
2026-01-07 22:09:45 -0800

    Initial Commit

---
+++ HelloIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloIOS
+//
+//  Created by Olivia Kim on 1/7/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HelloIOSTests/HelloIOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  HelloIOSTests.swift
+//  HelloIOSTests
+//
+//  Created by Olivia Kim on 1/7/26.
+//
+
+import Testing
+@testable import HelloIOS
+
+struct HelloIOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
